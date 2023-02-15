489423ad719d2c7078ac8ad45f6fc7698c6e4580
Vincent Radford <voodazzed@gmail.com>
2023-02-14 22:34:46 -0500

    VINCENGRADFORD PUSH

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("#keyInfo").slideToggle("slow");
+     
+    });
+  
+   
+
+  $("button#insights").click(function(){
+    $("#keyInfo2").slideToggle("slow");
+   
+  });
+
+
+  $("button#insights2").click(function(){
+    $("#keyInfo3").slideToggle("slow");
+   
+  });
+
+});   
+   
+  
